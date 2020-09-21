e8b952d4af771f566283ac37033535b2ea68c7c4
Aiden Marques <aiden.marques94@gmail.com>
2020-09-21 02:31:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+  if num < 2
+    false
+  elsif
+  (2..(num - 1)).each do |prime|
+    if (num % prime) == 0
+      return false
+    end
+  end
+  true
+end
+end
